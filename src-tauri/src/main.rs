0a19e84a10767c3a585c7f0369d22581c40165c6
TENMAJKL <silder.hologram@gmail.com>
2022-11-29 20:11:20 +0100

    adds

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,7 @@
+// epic rust programming
+// blazingly fast
+// parek
+
 #![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
@@ -5,14 +9,8 @@
 
 use serialport::SerialPort;
 
-struct Port {
-    port: Box<dyn SerialPort>
-}
-
-
-
-#[tauri::command]
-fn scan() -> bool {
+fn scan() -> Option<Box<dyn SerialPort>> {
+    // TODO return None instead of .expect()
     let ports = serialport::available_ports().expect("No ports found!");
     for p in ports {
         let port = serialport::new(p.port_name.as_str(), 115_200)
@@ -26,16 +24,47 @@ fn scan() -> bool {
             .read(result.as_mut_slice()).expect("Unable to read")
         ;
         
-        if String::from_utf8(result).expect("Bad string") == "csmoravak" {
-            return true;
+        if String::from_utf8(result).expect("menor cos to poslal") == "csmoravak" {
+            return Some(port);
         }
     }
-    return false;
+    return None;
+}
+
+fn scanProfiles(port: Box<dyn SerialPort>) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    for i in [1..5] {
+        let mut curent = vec![0; 32];
+        port.read(curent.as_mut_slice()).expect("Unable to read");
+        result.push(String::from_utf8(curent).expect("menor cos to poslal"));
+    }
+    return result;
+}
+
+#[tauri::command]
+fn write(setting: String) -> bool {
+    let port = scan();
+    if port == None {
+        return false;
+    }
+
+    scanProfiles(port);
+
+    return port.write(setting.as_bytes()) == None;
+}
+
+#[tauri::command]
+fn connect() -> Option<Vec<String>> {
+    let port = scan();
+    if port == None {
+        return None;
+    }
+
+    return scanProfiles(port);
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![scan])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
